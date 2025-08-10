df6fe23fbde5c3ac2b68bd72907127e30450a979
Quintec Music <quintek55555@gmail.com>
2025-08-10 16:18:03 -0700

    feat: session toggle for dev

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -3,12 +3,17 @@ use std::env;
 
 pub fn init() {
     dotenv().ok();
+
     env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    
     env::var("OAUTH_CLIENT_ID").expect("OAUTH_CLIENT_ID must be set");
     env::var("OAUTH_CLIENT_SECRET").expect("OAUTH_CLIENT_SECRET must be set");
     env::var("OAUTH_URL").expect("OAUTH_URL must be set");
     env::var("OAUTH_TOKEN_URL").expect("OAUTH_TOKEN_URL must be set");
     env::var("REDIRECT_URL").expect("REDIRECT_URL must be set");
+
     env::var("SESSION_SECRET").expect("SESSION_SECRET must be set");
+    env::var("SESSION_SECURE").expect("SESSION_SECURE must be set");
+    
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 }
