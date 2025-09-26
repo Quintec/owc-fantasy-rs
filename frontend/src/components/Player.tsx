2b68f9023542836bdeb49d030e8cce4ce9152f7c
sahijdev <sgill123@outlook.com>
2025-09-25 23:58:11 -0400

    Started work on player display for draft

---
+++ frontend/src/components/Player.tsx
@@ -0,0 +1,32 @@
+type PlayerProps = {
+    id: number;
+    username: string;
+    avatar_url: string;
+    country: string;
+    rank: number;
+    price: number;
+    eliminated: boolean;
+}
+
+export default function Player(player: PlayerProps) {
+
+    if (!player.username) {
+        return (
+            <div className="flex flex-col bg-pink-500 rounded-md p-5 max-w-1/4 m-5"> 
+                <h1>test</h1>
+            </div>
+        )
+    }
+
+    return (
+        <div className="flex flex-col bg-pink-500 rounded-md p-5 max-w-1/4 m-5"> 
+            <div className="flex flex-row bg-white-850 justify-between">
+                <img alt="avatar"></img>
+                <h1 className="text-white text-2xl font-bold">{player.username}</h1>
+                <img alt="country" src={player.avatar_url} className="max-w-1/8"></img>
+            </div>
+            <h1 className="text-white text-xl font-bold">Price: ${player.price}</h1>
+            <h1 className="text-white text-xl font-bold">Rank: {player.rank}</h1>
+        </div>
+    )
+}
