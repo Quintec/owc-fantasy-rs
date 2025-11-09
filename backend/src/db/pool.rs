2f5691db90071b631ac04b30013878ed4f2d1a5b
Quintec Music <quintek55555@gmail.com>
2025-11-09 15:21:29 -0800

    fix: db conf

--- backend/src/db/pool.rs
+++ backend/src/db/pool.rs
@@ -8,7 +8,6 @@ pub async fn create_pool() -> sqlx::MySqlPool {
     MySqlPoolOptions::new()
         .max_connections(5)
         .acquire_timeout(Duration::from_secs(30))
-        .connect_timeout(Duration::from_secs(30))
         .connect(&database_url)
         .await
         .expect("Error creating db pool")
