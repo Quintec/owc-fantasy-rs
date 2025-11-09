d23007b8beb1a48c6e0af1a3497c240839d6d8b9
Quintec Music <quintek55555@gmail.com>
2025-11-09 14:24:24 -0800

    feat: dockerize backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -34,7 +34,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(state::AppState { pool: pool.clone() }))
             .configure(routes::config)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
