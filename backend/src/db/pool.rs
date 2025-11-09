ab38c3996b86c9346165749cf292d4f766e39533
Quintec Music <quintek55555@gmail.com>
2025-11-09 15:16:17 -0800

    feat: pool logs

--- backend/src/db/pool.rs
+++ backend/src/db/pool.rs
@@ -1,9 +1,15 @@
 use sqlx::mysql::MySqlPoolOptions;
+use std::time::Duration;
 
 pub async fn create_pool() -> sqlx::MySqlPool {
+    let database_url = std::env::var("DATABASE_URL").unwrap();
+    println!("Attempting to connect to: {}", database_url);
+    
     MySqlPoolOptions::new()
         .max_connections(5)
-        .connect(&std::env::var("DATABASE_URL").unwrap())
+        .acquire_timeout(Duration::from_secs(30))
+        .connect_timeout(Duration::from_secs(30))
+        .connect(&database_url)
         .await
         .expect("Error creating db pool")
 }
